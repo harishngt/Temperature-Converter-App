503c612c3e8400438f4201da15196cfb154f423a
harishngt <97893600+harishngt@users.noreply.github.com>
2023-07-11 19:24:04 +0530

    Initialization

---
+++ lib/temperature.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TempConv extends StatefulWidget {
+  const TempConv({super.key});
+
+  @override
+  State<TempConv> createState() => _TempConvState();
+}
+
+class _TempConvState extends State<TempConv> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
