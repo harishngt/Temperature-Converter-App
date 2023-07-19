488123a95d758fc1f634b2a27a4a980350f1eb22
harishngt <97893600+harishngt@users.noreply.github.com>
2023-07-19 22:31:39 +0530

    UI Completed

--- lib/temperature.dart
+++ lib/temperature.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:temperature_converter_app/theme/color.dart';
+import 'package:temperature_converter_app/widgets/radiobutton.dart';
 
 class TempConv extends StatefulWidget {
   const TempConv({super.key});
@@ -9,25 +10,145 @@ class TempConv extends StatefulWidget {
 }
 
 class _TempConvState extends State<TempConv> {
+  List tempunitss = [
+    'Celsius',
+    'Kelvin',
+    'Fahrenheit',
+  ];
+  String temp = '';
+  String currentin = 'Celsius';
+  String currentout = 'Fahrenheit';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           "Temperature Converter",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.white, fontSize: 25),
+          style: TextStyle(color: Colors.white),
         ),
         backgroundColor: AppColor.primary,
-        toolbarHeight: 75,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currentin,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  height: 35,
+                  width: 180,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    style: TextStyle(fontSize: 20),
+                    maxLines: 1,
+                    minLines: 1,
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'From',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey.shade700,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (int i = 0; i < tempunitss.length; i++)
+                        CustomRadioButton(
+                          unit: tempunitss[i],
+                          currentlySelected: currentin,
+                          onTap: (unit) {
+                            currentin = unit;
+                            setState(() {});
+                          },
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currentout,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  height: 35,
+                  width: 180,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    style: TextStyle(fontSize: 20),
+                    maxLines: 1,
+                    minLines: 1,
+                    decoration: InputDecoration(
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'To',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey.shade700,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (int i = 0; i < tempunitss.length; i++)
+                        CustomRadioButton(
+                          unit: tempunitss[i],
+                          currentlySelected: currentout,
+                          onTap: (unit) {
+                            currentout = unit;
+                            setState(() {});
+                          },
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.swap_horiz_sharp),
-        onPressed: () {},
+        onPressed: () {
+          temp = currentin;
+          currentin = currentout;
+          currentout = temp;
+          setState(() {});
+        },
         foregroundColor: AppColor.secondary,
         backgroundColor: AppColor.primary,
       ),
-      body: Container(),
     );
   }
 }
---
+++ lib/widgets/radiobutton.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:temperature_converter_app/theme/color.dart';
+
+class CustomRadioButton extends StatelessWidget {
+  final String unit;
+  final String currentlySelected;
+  final Function? onTap;
+  const CustomRadioButton({
+    super.key,
+    required this.unit,
+    this.onTap,
+    required this.currentlySelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap!(unit);
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: [
+            Container(
+              height: 20,
+              width: 20,
+              child: Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: Container(
+                  height: 15,
+                  width: 15,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: (currentlySelected == unit)
+                        ? (AppColor.primary)
+                        : (Colors.transparent),
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                border: Border.all(
+                  color: AppColor.primary,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              unit,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
