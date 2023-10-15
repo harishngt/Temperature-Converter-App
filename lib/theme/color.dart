a0b7ee387f5452eaa511595603fa56bec8813d10
harishngt <97893600+harishngt@users.noreply.github.com>
2023-10-15 19:59:56 +0530

    App Icon and App Name added

--- lib/theme/color.dart
+++ lib/theme/color.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const primary = Color(0xFFF94449);
+  static const primary = Color(0xFFf20a51);
   static var secondary = Color(0xFFFFFFFF);
 }
